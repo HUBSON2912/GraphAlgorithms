af6154a7cbb1c4c5e19972b85af1b680e0d7547c
Hubert Słowikowski <hubert.slowikowski.2912@gmail.com>
2025-05-19 23:02:55 +0200

    input, doesnt work if there is a cycle A->B->A (probably)

--- ford fulkerson.py
+++ ford fulkerson.py
@@ -2,41 +2,29 @@ import queue
 
 przeplyw_max=0
 
-# połączenie --> [docelowy, przepływ, pojemność max]
-# graf=[
-#     {"numer": 0, "polaczenia": [[1,0,10], [2,0,11]]},
-#     {"numer": 1, "polaczenia": [[2,0,15], [3,0,7]]},
-#     {"numer": 2, "polaczenia": [[3,0,8], [4,0,3]]},
-#     {"numer": 3, "polaczenia": [[5,0,9]]},
-#     {"numer": 4, "polaczenia": [[5,0,14]]},
-#     {"numer": 5, "polaczenia": []},
-# ]
-
-graf=[
-    {"numer": 0, "polaczenia": [[1,0,4]]},
-    {"numer": 1, "polaczenia": [[2,0,1], [3,0,2], [5,0,3]]},
-    {"numer": 2, "polaczenia": [[3,0,2], [4,0,6]]},
-    {"numer": 3, "polaczenia": [[0,0,1], [6,0,1]]},
-    {"numer": 4, "polaczenia": [[6,0,9]]},
-    {"numer": 5, "polaczenia": [[2,0,3], [0,0,3]]},
-    {"numer": 6, "polaczenia": []}
-]
+# zawartość grafu {"numer": INDEX, "polaczenia": [ [CEL, PRZEPLYW, POJEMNOSC] ]}
+graf=[]
 
 def ile_jeszcze_moze_plynac(skad, dokad):
     polaczenia=graf[skad]["polaczenia"]
     for indeks, przeplyw, pojemnosc in polaczenia:
-        # indeks, przeplyw, pojemnosc=
         if indeks==dokad:
             return pojemnosc-przeplyw
     return -1
 
-def bfs(start:int, koniec:int):
+
+# znajduje ścieżkę do każdego wierzchołka od startu
+# tak że nie przechodzi przez krawędzie którymi już 
+# nic nie może przepłynąć
+def bfs(start):
     kolejka=queue.Queue()
     odwiedzony=[False]*len(graf)
     skad_przyszedlem=[-1]*len(graf) # -1 - nie da się, -2 - jestem początkiem
     skad_przyszedlem[start]=-2
+
     kolejka.put(start)
     odwiedzony[start]=True
+
     while not kolejka.empty():
         obecny=kolejka.get()
         polaczenia=graf[obecny]["polaczenia"]
@@ -47,7 +35,8 @@ def bfs(start:int, koniec:int):
                     skad_przyszedlem[indeks_sasiada]=obecny
     return skad_przyszedlem
 
-def daj_przejscie(skad_przyszedlem, koniec):
+
+def daj_sciezke(skad_przyszedlem, koniec):
     sciezka=[]
     obecny=koniec
     while obecny!=-2:
@@ -56,6 +45,7 @@ def daj_przejscie(skad_przyszedlem, koniec):
     sciezka=list(reversed(sciezka))
     return sciezka
 
+
 def znajdz_minimum(sciezka):
     minimum=2**31-1
     for i in range(len(sciezka)-1):
@@ -63,6 +53,7 @@ def znajdz_minimum(sciezka):
         przeplyw=ile_jeszcze_moze_plynac(obecny, nastepny)
         minimum=min(minimum, przeplyw)
     return minimum
+
 
 def aktualizuj_przeplywy_na_sciezce(sciezka, dodaj_przeplyw):
     global przeplyw_max
@@ -82,24 +73,39 @@ def aktualizuj_przeplywy_na_sciezce(sciezka, dodaj_przeplyw):
         for i in range(len(polaczenia_nastepnego)):
             if polaczenia_nastepnego[i][0]==obecny:
                 polaczenia_nastepnego[i][1]-=dodaj_przeplyw
-        else:
+                break
+        else:  # jeśli nie ma połączenia wstecz to dodaj takowe 
             polaczenia_nastepnego.append([obecny, -dodaj_przeplyw, 0])
 
-def fordfulkerson(zrodlo: int, ujscie: int):
-    skad_przyszedlem=bfs(zrodlo, ujscie)
+
+def fordfulkerson(zrodlo, ujscie):
+    skad_przyszedlem=bfs(zrodlo)
     
     #dopóki istnieje przejście
     while skad_przyszedlem[ujscie]!=-1:
-        sciezka=daj_przejscie(skad_przyszedlem, ujscie)
+        sciezka=daj_sciezke(skad_przyszedlem, ujscie)
         ile_przeplynie_przez_sciezke=znajdz_minimum(sciezka)
         aktualizuj_przeplywy_na_sciezce(sciezka,ile_przeplynie_przez_sciezke)
-        skad_przyszedlem=bfs(zrodlo, ujscie)
+        skad_przyszedlem=bfs(zrodlo)  # znajdź nową ścieżkę
 
-zrodlo=5
-ujscie=6
+
+
+
+print("WIERZCHOLKI SA NUMEROWANE OD 0\n\n")
+n=int(input("Podaj ilosc wierzcholkow: "))
+for i in range(n):
+    graf.append({"numer": i, "polaczenia": []})
+
+
+m=int(input("Podaj ilosc krawedzi: "))
+for i in range(m):
+    skad, dokad, pojemnosc=input("Podaj wierzcholek poczatkowy, koncowy i pojemnosc tej krawedzi: ").split()
+    skad, dokad, pojemnosc = int(skad), int(dokad), int(pojemnosc)
+    graf[skad]["polaczenia"].append([dokad, 0, pojemnosc])
+
+zrodlo = int(input("Podaj zrodlo: "))
+ujscie = int(input("Podaj ujscie: "))
 
 fordfulkerson(zrodlo, ujscie)
-# for indeks, przeplyw, pojemnosc in graf[ujscie]["polaczenia"]:
-#     przeplyw_max+=(przeplyw if przeplyw>0 else 0)
 
 print(przeplyw_max)
